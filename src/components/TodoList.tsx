c4dfe48b19d6e3865e7648433703c5105389a83e
Owk <kkoctuk2005@gmail.com>
2025-09-01 23:43:42 +0300

    task solution

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,42 +1,78 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Todo } from '../types/Todo';
+import classNames from 'classnames';
 
 type Props = {
   todos: Todo[] | null;
 };
 
 export const TodoList: React.FC<Props> = ({ todos }) => {
+  const [loading, setLodaing] = useState(false);
+  const [editTodoId, setEditTodoId] = useState(0);
+
+  const handleEdit = (todoId: number) => {
+    setEditTodoId(todoId);
+  };
+
   return (
     <section className="todoapp__main" data-cy="TodoList">
-      {/* This is a completed todo */}
-      <div data-cy="Todo" className="todo completed">
-        <label className="todo__status-label">
-          <input
-            data-cy="TodoStatus"
-            type="checkbox"
-            className="todo__status"
-            checked
-          />
-        </label>
+      {todos?.map(todo => (
+        <div
+          data-cy="Todo"
+          className={classNames('todo', { 'todo completed': todo.completed })}
+          key={todo.id}
+        >
+          <label className="todo__status-label">
+            <input
+              data-cy="TodoStatus"
+              type="checkbox"
+              className="todo__status"
+              checked={todo.completed}
+              onDoubleClick={() => handleEdit(todo.id)}
+              onBlur={() => setEditTodoId(0)}
+            />
+          </label>
 
-        <span data-cy="TodoTitle" className="todo__title">
-          Completed Todo
-        </span>
+          {todo.id === editTodoId ? (
+    <form>
+    <input
+      data-cy="TodoTitleField"
+      type="text"
+      className="todo__title-field"
+      placeholder="Empty todo will be deleted"
+      value="Todo is being edited now"
+    />
+  </form>
+) : (
+  <>
+    <span data-cy="TodoTitle" className="todo__title">
+      {todo.title}
+    </span>
 
-        {/* Remove button appears only on hover */}
-        <button type="button" className="todo__remove" data-cy="TodoDelete">
-          ×
-        </button>
+    {/* Remove button appears only on hover */}
+    <button
+      type="button"
+      className="todo__remove"
+      data-cy="TodoDelete"
+    >
+      ×
+    </button>
+  </>
+)}
 
-        {/* overlay will cover the todo while it is being deleted or updated */}
-        <div data-cy="TodoLoader" className="modal overlay">
-          <div className="modal-background has-background-white-ter" />
-          <div className="loader" />
+          {/* overlay will cover the todo while it is being deleted or updated */}
+          <div
+            data-cy="TodoLoader"
+            className={classNames('modal overlay', { 'is-active': loading })}
+          >
+            <div className="modal-background has-background-white-ter" />
+            <div className="loader" />
+          </div>
         </div>
-      </div>
+      ))}
 
       {/* This todo is an active todo */}
       <div data-cy="Todo" className="todo">
@@ -55,7 +91,10 @@ export const TodoList: React.FC<Props> = ({ todos }) => {
           ×
         </button>
 
-        <div data-cy="TodoLoader" className="modal overlay">
+        <div
+          data-cy="TodoLoader"
+          className={classNames('modal overlay', { 'is-active': loading })}
+        >
           <div className="modal-background has-background-white-ter" />
           <div className="loader" />
         </div>
